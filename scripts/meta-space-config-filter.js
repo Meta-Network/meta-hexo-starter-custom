c0a2ec09222dd4eef88fc057de1d40b9feedeb7f
FZxiao233 <fzxiao@outlook.com>
2021-10-27 21:52:29 +0800

    chore: Configure theme freemind

---
+++ scripts/meta-space-config-filter.js
@@ -0,0 +1,24 @@
+const fs = require('hexo-fs');
+const { deepMerge } = require('hexo-util');
+const path = require('path');
+
+// eslint-disable-next-line no-undef
+hexo.extend.filter.register('after_init', async function () {
+  const { base_dir, render } = this;
+
+  const metaConfigPath = path.join(base_dir, 'meta-space-config.yml');
+  const isExists = await fs.exists(metaConfigPath);
+  if (!isExists) return;
+
+  const metaConfig = await render.render({ path: metaConfigPath });
+  const { user, site } = metaConfig;
+  let freemindConfig = {};
+  if (site && user) {
+    freemindConfig = {
+      favicon: site.favicon,
+      slogan: site.subtitle
+    }
+  }
+
+  this.config.theme_config = deepMerge(this.config.theme_config, fluidConfig);
+});
