47be14593fe6fd31575b74327be463f2863ec6b5
WhyYouAre <whyouare556@protonmail.com>
2021-11-01 16:50:14 +0800

    chore: Get IPFS Gateway URL from metaSpaceConfig

--- scripts/ipfs-image-renderer.js
+++ scripts/ipfs-image-renderer.js
@@ -1,4 +1,6 @@
 hexo.extend.filter.register("markdown-it:renderer", md => {
+    const { metaSpaceConfig } = hexo.config;
+    const { gateway } = metaSpaceConfig;
     const originalHandler = md.renderer.rules.image;
 
     md.renderer.rules.image = function (tokens, idx, options, env, self) {
@@ -6,7 +8,7 @@ hexo.extend.filter.register("markdown-it:renderer", md => {
 
         const sourceUrl = new URL(token.attrGet("src"));
         if (sourceUrl.protocol === "ipfs:") {
-            token.attrSet("src", `https://ipfs.fleek.co/ipfs/${sourceUrl.hostname}`);
+            token.attrSet("src", `${gateway.ipfs.baseUrl}/${sourceUrl.hostname}`);
         }
 
         return originalHandler.call(this, tokens, idx, options, env, self);
