d47355e33b5d7edcc60b2c59ec4293c83c30d0b0
FZxiao233 <fzxiao@outlook.com>
2021-11-09 20:40:40 +0800

    fix: 404 at pages of navbar

--- install_theme.sh
+++ install_theme.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/zthxxx/hexo-theme-Wikitten themes/Wikitten 
+
+cp -rf themes/Wikitten/_source/* source/
+cp -rf themes/Wikitten/_scaffolds/* scaffolds/
