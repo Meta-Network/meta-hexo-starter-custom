f9574320e2209981bd97765f017fbc05310440d8
FZxiao233 <fzxiao@outlook.com>
2021-11-25 20:41:19 +0800

    chore: Get metaSpaceConfig from hexo config

--- scripts/meta-space-config-filter.js
+++ scripts/meta-space-config-filter.js
@@ -1,17 +1,10 @@
-const fs = require('hexo-fs');
 const { deepMerge } = require('hexo-util');
-const path = require('path');
 
 // eslint-disable-next-line no-undef
 hexo.extend.filter.register('after_init', async function () {
-  const { base_dir, render } = this;
+  const { metaSpaceConfig } = this.config;
 
-  const metaConfigPath = path.join(base_dir, 'meta-space-config.yml');
-  const isExists = await fs.exists(metaConfigPath);
-  if (!isExists) return;
-
-  const metaConfig = await render.render({ path: metaConfigPath });
-  const { user, site } = metaConfig;
+  const { user, site } = metaSpaceConfig;
   let yiliaConfig = {};
   if (site && user) {
     yiliaConfig = {
