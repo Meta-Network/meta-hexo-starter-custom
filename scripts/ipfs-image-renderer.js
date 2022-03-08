8c82d4fbb777e9c6296770965bc78746173b7db9
Garfield Lee <Garfield550@users.noreply.github.com>
2022-03-08 15:07:14 +0800

    fix: use regexp check ipfs protocol...
    and use meta space config ipfs gatway url by default

--- scripts/ipfs-image-renderer.js
+++ scripts/ipfs-image-renderer.js
@@ -1,14 +1,20 @@
 hexo.extend.filter.register("markdown-it:renderer", md => {
     const originalHandler = md.renderer.rules.image;
+    const checkIPFS = /^ipfs:\/\//;
 
-    md.renderer.rules.image = function (tokens, idx, options, env, self) {
+    function buildIPFSUrl(cid) {
+        const defaultUrl = "https://ipfs.fleek.co/ipfs/";
+        const baseUrl = hexo.config?.metaSpaceConfig?.gateway?.ipfs?.baseUrl || defaultUrl;
+        return `${baseUrl}${cid}`
+    }
+
+    md.renderer.rules.image = function processIPFSImage(tokens, idx, options, env, self) {
         const token = tokens[idx];
-
-        const sourceUrl = new URL(token.attrGet("src"));
-        if (sourceUrl.protocol === "ipfs:") {
-            token.attrSet("src", `https://ipfs.fleek.co/ipfs/${sourceUrl.hostname}`);
+        const tokenSrc = token.attrGet("src");
+        if (checkIPFS.test(tokenSrc)) {
+            const sourceUrl = new URL(tokenSrc);
+            token.attrSet("src", buildIPFSUrl(sourceUrl.hostname));
         }
-
         return originalHandler.call(this, tokens, idx, options, env, self);
     };
 });
